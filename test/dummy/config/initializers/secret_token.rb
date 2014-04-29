c8b326a2206e70366614ec7fa9715036d8a510e6
kari <mr.kari.ikonen@gmail.com>
2014-04-29 20:15:26 +0300

    INIT: test app

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'cbdbd11b1230075056c853c147853431474dcb1b22dd26e9eb56b2b2a9659c006d0876a551db58ed23617a44ea264a4b29d5c674d5b5ec088385f13f15b2e841'
---
+++ test/sampler_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SamplerTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Sampler
+  end
+end
