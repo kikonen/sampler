8bda6672437ff9c2653ab1a1124528c01588a403
kari <mr.kari.ikonen@gmail.com>
2014-05-05 23:29:48 +0300

    Separate login page - ADD: login page - FIX: target server is stored globally in service

--- app/assets/javascripts/sampler/auth/auth_service.js
+++ app/assets/javascripts/sampler/auth/auth_service.js
@@ -1,55 +1,64 @@
 "use strict";
 angular
   .module('sampler.auth', ['ng-auth'] )
-  .service(
+  .factory(
     "AuthenticationService",
-     function($http, $cookies, $cookieStore, auth) {
-       var basic = auth.get('basic');
+    function($http, $cookies, $cookieStore, auth) {
+      var basic = auth.get('basic');
 
-       var state = {
-         server: '',
-         username: ''
-       };
+      var state = {
+        url: 'http://localhost:3000',
+        api: '/auth/token',
+        username: ''
+      };
 
-       this.authenticated = function() {
-         return !!$cookies.auth_token;
-       };
+      function login(username, password) {
+        console.log(state);
+        $cookieStore.remove('auth_token');
+        state.username = username;
 
-       this.login = function(server, username, password, api) {
-         $cookieStore.remove('auth_token');
-         state.username = username;
-         state.server = server;
+        basic.credentials(username, password);
+        var url = "" + state.url + state.api;
+        console.log("login: " + url);
 
-         basic.credentials(username, password);
-         var url = server + api;
-         console.log(server);
-         console.log("login: " + url);
+        basic.$http({method: 'GET', url: url, withCredentials: true, Authentication: 'xxx:yyy'})
+          .success(function(data, status, headers, config) {
+            console.log(status);
+            console.log($cookies);
+          })
+          .error(function(data, status, headers, config) {
+            console.log(status);
+            console.log("login: FAIL");
+          });
+      };
 
-         basic.$http({method: 'GET', url: url, withCredentials: true, Authentication: 'xxx:yyy'})
-           .success(function(data, status, headers, config) {
-             console.log(status);
-             console.log("login: SUCCESS");
-             console.log($cookies);
-           })
-           .error(function(data, status, headers, config) {
-             console.log(status);
-             console.log("login: FAIL");
-           });
-       };
+      function logout() {
+        var url = state.url + state.api;
+        console.log("logout: " + url);
 
-       this.logout = function(api) {
-         console.log("logout: " + state.server);
-         var url = state.server + api;
-         console.log("logout: " + url);
+        basic.$http({method: 'DELETE', url: url})
+          .success(function(data, status, headers, config) {
+            $cookieStore.remove('auth_token');
+            basic.forgetCredentials();
+          })
+          .error(function(data, status, headers, config) {
+            $cookieStore.remove('auth_token');
+            basic.forgetCredentials();
+          });
+      };
 
-         basic.$http({method: 'DELETE', url: url})
-           .success(function(data, status, headers, config) {
-             $cookieStore.remove('auth_token');
-             basic.forgetCredentials();
-           })
-           .error(function(data, status, headers, config) {
-             $cookieStore.remove('auth_token');
-             basic.forgetCredentials();
-      });
-       };
-     });
+      return {
+        setServer: function(url, api) {
+          state.url = url;
+          state.api = api;
+        },
+        getUrl: function() {
+          return state.url;
+        },
+        getApi: function() {
+          return state.api;
+        },
+        login: login,
+        logout: logout
+      };
+    });
